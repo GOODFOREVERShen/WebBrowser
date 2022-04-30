7f6dc26801eb18c42a7a16e435d61fcf8b3a1523
Maurice-1235 <58433591+Maurice-1235@users.noreply.github.com>
2022-04-30 15:28:15 +0700

    initialize repo

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stddef.h>
+#include <windows.h>
+#include <olectl.h>
+#include <mmsystem.h>
+#include <wingdi.h>
+#include <ole2.h>
+#include <ocidl.h>
+#include <winuser.h>
+#include <conio.h>
+
+#include "./libgraphics/graphics.h"
+#include "./libgraphics/extgraph.h"
+#include "./libgraphics/genlib.h"
+#include "./libgraphics/simpio.h"
+#include "./libgraphics/strlib.h"
+#include "./libgraphics/linkedlist.h"
+#include "./libgraphics/imgui.h"
+
+void Main()
+{
+    
+}
